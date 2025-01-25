cb9ea20ef08d93f2e0076bc961cf1c628c3d051e
Ваше Имя <.email@example.com>
2025-01-25 18:44:45 +0500

    first commit

---
+++ Index.cpp
@@ -0,0 +1,6 @@
+#include <WiFi.h>
+#include <UniversalTelegramBot.h>
+
+// Замените эти значения на ваши реальные токены и идентификаторы
+#define BOT_TOKEN "YOUR_DEFAULT_BOT_TOKEN"
+#define CHAT_ID "YOUR_DEFAULT_CHAT_ID"
