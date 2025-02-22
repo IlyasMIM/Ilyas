10de6d5c347ef3189edb15ea78c21e1599fc616e
MIM4291Nomad <mim4291@mail.ru>
2025-02-22 15:01:36 +0500

    3commit

--- Index.cpp
+++ Index.cpp
@@ -1,6 +1,6 @@
 #include <WiFi.h>
 #include <UniversalTelegramBot.h>
-
+//добавил
 // ЗаДЖИГУРДАмените эти значения на ваши реальные токены и идентификаторы
 #define BOT_TOKEN "YOUR_DEFAULT_BOT_TOKEN"
 #define CHAT_ID "YOUR_DEFAULT_CHAT_ID"
