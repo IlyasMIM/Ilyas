08071df0376a0169acff970a037a916d76de0128
Ilyas <mim4291@mail.ru>
2025-02-22 12:33:23 +0500

    second_commit

--- Index.cpp
+++ Index.cpp
@@ -1,7 +1,7 @@
 #include <WiFi.h>
 #include <UniversalTelegramBot.h>
 
-// Замените эти значения на ваши реальные токены и идентификаторы
+// ЗаДЖИГУРДАмените эти значения на ваши реальные токены и идентификаторы
 #define BOT_TOKEN "YOUR_DEFAULT_BOT_TOKEN"
 #define CHAT_ID "YOUR_DEFAULT_CHAT_ID"
 ciout <<"GOPA";
